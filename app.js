d1e9414ce90d8e9aaf9479df8fd0c0d9651da930
Miguel Martinez <logistica@tecindes.com>
2025-02-10 12:49:32 -0500

    Desafio3

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let _amigos=[];
+let _areaSeleccionada=0;
 
 function agregarAmigo(){
     let _nombreIngresado=document.getElementById('amigo');
@@ -54,7 +55,7 @@ function sortearAmigo(){
     //return ( Math.floor(Math.random() * (_indeceMaximo - _indiceMinimo + 1) + _indiceMinimo))+1;
 }
 
-function prueba7DiasOfCode(){
+function _1De7DiasOfCode(){
     let _numeroUno=1;
     let _stringUno='1';
     let _numeroTreinta=30;
@@ -76,6 +77,31 @@ function prueba7DiasOfCode(){
         console.log('Las variables _numeroDiez y _stringDiez tienen el mismo valor, pero tipos diferentes');
     } else {
         console.log('Las variables _numeroDiez y _stringDiez no tienen el mismo valor');
+    }
+}
+
+function _2De7DiasOfCode(){
+    const _nombre =prompt("¿Cúal es tu nombre?");
+    const _edad =prompt("¿Cuántos años tienes?");
+    const _lenguaje=prompt("¿Qué lenguaje de programación estas estudiando?");
+    const _mensaje=`¡Cómo estás ${_nombre}, veo que tienes ${_edad} años y te encuentras estudiando ${_lenguaje}!`;
+    alert(_mensaje);
+    const  _preguntaTeGusta=prompt(`¿ Responde 1 para sí y 2 para no, si te gusta estudiar ${_lenguaje}`);
+    if (_preguntaTeGusta>=1 && _preguntaTeGusta<=2){
+        alert(`${_preguntaTeGusta==1 ?'¡Muy bien sigue estudiando!':'Intenta con otro lenguaje'}`);
+    } 
+}
+
+function _3De7DiasOfCode(_areaSeleccionada){
+    let _continuarTecnologias =1;
+    if (_areaSeleccionada===1 || _areaSeleccionada===2){
+        const _eleccion=prompt(`${_areaSeleccionada==1 ? 'Selecciono Front-End, ':'Selecciono Back-End, '} ${_areaSeleccionada==1 ? 'Selecciona 1 para React, 2 para Vue' : 'Selecciona 1 para C#, 2 para Java'}`,1);
+        const _especialidad=prompt("Para especializarce en el área elegida dígite 1, para convertirse en Full Stack digite 2",1);
+        while(_continuarTecnologias===1){
+            const _respuesta=prompt("¿Hay alguna otra tecnología que te gustaría aprender?, responder Ok","Ok");
+            _continuarTecnologias=`${_respuesta=='Ok' ? 1:2}`;
+            alert(_continuarTecnologias);
+        }
     }
 }
  
